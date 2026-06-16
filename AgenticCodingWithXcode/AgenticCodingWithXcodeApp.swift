27c49fd182d239ce9de8c77b794e1ae4431966de
Gino Alo <gino.simon.alo@gmail.com>
2026-06-16 22:30:23 +0800

    Initial Commit

---
+++ AgenticCodingWithXcode/AgenticCodingWithXcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgenticCodingWithXcodeApp.swift
+//  AgenticCodingWithXcode
+//
+//  Created by Gino Alo on 6/16/26.
+//
+
+import SwiftUI
+
+@main
+struct AgenticCodingWithXcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AgenticCodingWithXcodeTests/AgenticCodingWithXcodeTests.swift
@@ -0,0 +1,18 @@
+//
+//  AgenticCodingWithXcodeTests.swift
+//  AgenticCodingWithXcodeTests
+//
+//  Created by Gino Alo on 6/16/26.
+//
+
+import Testing
+
+struct AgenticCodingWithXcodeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
